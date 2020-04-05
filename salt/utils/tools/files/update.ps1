377295bbd7325a74bea7db623753be1932c8d6fd
Ghislain LE MEUR <gigi206+github@gmail.com>
2020-04-05 15:58:37 +0200

    Fix issues update.ps1

--- salt/utils/tools/files/update.ps1
+++ salt/utils/tools/files/update.ps1
@@ -1,3 +1,30 @@
+# Output actions to console
+function Output([string]$msg, [bool]$newline=$true, [string]$fgcolor='Cyan', [bool]$star=$true) {
+    if (${star}) { Write-Host "* " -nonewline -ForegroundColor Green }
+    if (${newline}) { Write-Host "${msg}" -ForegroundColor ${fgcolor} }
+    else { Write-Host -nonewline "${msg}" -ForegroundColor ${fgcolor} }
+}
+
+# Output errors to console
+function OutputErrror([string]$msg, [bool]$newline=$true, [bool]$exit=$true, [int]$exit_code=1) {
+    if (${exit}) { ${message} = "A critical error has occurred: ${msg}" }
+    else { ${message} = "An error has occurred (not critical): ${msg}" }
+
+    if (${newline}) { Write-Host "${message}" -ForegroundColor Red }
+    else { Write-Host -nonewline "${message}" -ForegroundColor Red }
+
+    if (${exit}) {
+        if (!(${env:VSCode_Anywhere_CI})) { Pause }
+        exit ${exit_code}
+    }
+}
+
+function RunCmd([String]$Cmd, [String]$ArgumentList=${null}, [String]$WorkingDirectory=(Get-Location).Path, [Bool]$Admin=${false}) {
+    if (${Admin}) { $process = Start-Process -PassThru -Wait -WorkingDirectory "${WorkingDirectory}" -FilePath "${Cmd}" -ArgumentList "${ArgumentList}" -Verb "RunAs" }
+    else { $process = Start-Process -PassThru -Wait -NoNewWindow -WorkingDirectory "${WorkingDirectory}" -FilePath "${Cmd}" -ArgumentList "${ArgumentList}" }
+    if (${process}.ExitCode -ne 0) { OutputErrror "failed to run command: ${Cmd} ${ArgumentList}" }
+}
+
 Write-Host ""                                                         -ForegroundColor Green
 Write-Host "/=======================================================" -ForegroundColor Green
 Write-Host "|"                                                        -ForegroundColor Green
@@ -12,53 +39,59 @@ Set-Location "${PSScriptRoot}"
 {%- if salt['grains.get']('vscode-anywhere:profile') == 'windows_admin' %}
 $currentPrincipal = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
 if (!($currentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator))) {
-    Write-Host "You must execute this script with the administrator rights"
-    $run = Start-Process -NoNewWindow -PassThru -Wait powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs
-    $exit = $run.ExitCode
+    Output "You must execute this script with the administrator rights"
+    RunCmd -Cmd "powershell.exe" -ArgumentList "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Admin ${true}
 }
 {%- endif %}
 
-# Set-Location ((Resolve-Path "${PSScriptRoot}\{{ salt['vscode_anywhere.relpath'](salt['grains.get']('vscode-anywhere:tools:path'), salt['grains.get']('vscode-anywhere:saltstack:path')) }}").Path + '\bin')
-# Set-Location ("{{ salt['vscode_anywhere.relpath'](salt['grains.get']('vscode-anywhere:tools:path'), salt['grains.get']('vscode-anywhere:saltstack:path')) }}" + '\bin')
-Write-Host "* Updating VSCode-Anywhere from branch {{ saltenv }}" -ForegroundColor Cyan
-Write-Host "* Updating saltstack pillar file" -ForegroundColor Cyan
+Output "Updating VSCode-Anywhere from branch {{ saltenv }}"
+Output "Updating saltstack pillar file"
 Invoke-WebRequest -Uri https://raw.githubusercontent.com/gigi206/VSCode-Anywhere/{{ saltenv }}/salt/conf/srv/pillar/saltstack.sls -OutFile "{{ salt['grains.get']('vscode-anywhere:saltstack:pillar_path')}}\saltstack.sls"
 # $saltCurrentVersion = & ("{{ salt['vscode_anywhere.relpath'](salt['grains.get']('vscode-anywhere:tools:path'), salt['grains.get']('vscode-anywhere:saltstack:path')) }}" + '\bin\python.exe') -c "import salt.version;print(salt.version.__version_info__[0])"
 $saltCurrentVersion = & ("{{ salt['vscode_anywhere.relpath'](salt['grains.get']('vscode-anywhere:tools:path'), salt['grains.get']('vscode-anywhere:saltstack:path')) }}" + '\bin\python.exe') -c "import salt.version;print('.'.join(str(v) for v in salt.version.__version_info__[0:2]))"
 $saltTargetVersion = (& "${PSScriptRoot}\vscode-anywhere.ps1" pillar.get saltstack:version --out json | ConvertFrom-Json).local
 if ($saltCurrentVersion -ne $saltTargetVersion) {
-    Write-Host "* Updating Saltsatck ${saltCurrentVersion} => ${saltTargetVersion}" -ForegroundColor Cyan
-    Invoke-WebRequest -Uri https://repo.saltstack.com/windows/Salt-Minion-${saltTargetVersion}.0-Py3-AMD64-Setup.exe -OutFile "{{ salt['vscode_anywhere.relpath'](salt['grains.get']('vscode-anywhere:tools:path'), salt['grains.get']('vscode-anywhere:apps:path')) }}\saltstack.exe"
+    Output "Updating Saltsatck ${saltCurrentVersion} => ${saltTargetVersion}"
+    Invoke-WebRequest -Uri https://repo.saltstack.com/windows/Salt-Minion-${saltTargetVersion}-Py3-AMD64-Setup.exe -OutFile "{{ salt['vscode_anywhere.relpath'](salt['grains.get']('vscode-anywhere:tools:path'), salt['grains.get']('vscode-anywhere:apps:path')) }}\saltstack.exe"
     Remove-Item -Path "{{ salt['vscode_anywhere.relpath'](salt['grains.get']('vscode-anywhere:tools:path'), salt['grains.get']('vscode-anywhere:saltstack:path')) }}" -Recurse -Force
-    Start-Process -Wait -NoNewWindow -WorkingDirectory "{{ salt['vscode_anywhere.relpath'](salt['grains.get']('vscode-anywhere:tools:path'), salt['grains.get']('vscode-anywhere:apps:path')) }}" -FilePath "7z.exe" -ArgumentList "x `"{{ salt['vscode_anywhere.relpath'](salt['grains.get']('vscode-anywhere:tools:path'), salt['grains.get']('vscode-anywhere:apps:path')) }}\saltstack.exe`" -y"
-    Write-Host "* Syncing Saltstack to the latest VSCode-Anywhere updates (fork)" -ForegroundColor Cyan
-    # Set-Location ((Resolve-Path "${PSScriptRoot}\{{ salt['vscode_anywhere.relpath'](salt['grains.get']('vscode-anywhere:tools:path'), salt['grains.get']('vscode-anywhere:saltstack:path')) }}").Path + '\bin\Lib\site-packages\salt-*')
-    Set-Location ("{{ salt['vscode_anywhere.relpath'](salt['grains.get']('vscode-anywhere:tools:path'), salt['grains.get']('vscode-anywhere:saltstack:path')) }}" + '\bin\Lib\site-packages\salt-*')
+    New-Item -Path "{{ salt['vscode_anywhere.relpath'](salt['grains.get']('vscode-anywhere:tools:path'), salt['grains.get']('vscode-anywhere:saltstack:path')) }}" -ItemType "directory" | Out-Null
+    RunCmd -Cmd 7z.exe -ArgumentList "x `"{{ salt['grains.get']('vscode-anywhere:apps:path') | path_join('saltstack.exe') }}`" -y" -WorkingDirectory "{{ salt['grains.get']('vscode-anywhere:saltstack:path') }}"
+    Remove-Item "{{ salt['vscode_anywhere.relpath'](salt['grains.get']('vscode-anywhere:tools:path'), salt['grains.get']('vscode-anywhere:apps:path')) | path_join('saltstack.exe') }}"
+    Output "Syncing Saltstack to the latest VSCode-Anywhere updates (fork)"
+    Set-Location ("{{ salt['vscode_anywhere.relpath'](salt['grains.get']('vscode-anywhere:tools:path'), salt['grains.get']('vscode-anywhere:saltstack:path')) }}" + "\bin\Lib\site-packages\salt-*")
     Rename-Item -Path salt -NewName salt.orig
-    & git init .
-    & git config core.sparsecheckout true
+    RunCmd -Cmd git.exe -ArgumentList "init ."
+    RunCmd -Cmd git.exe -ArgumentList "config core.sparsecheckout true"
     Add-Content -Path ".git/info/sparse-checkout" -Value "salt/*"
     Add-Content -Path ".gitignore" -Value "*"
     Add-Content -Path ".gitignore" -Value "!salt"
-    & git remote add origin https://github.com/gigi206/salt.git
-    & git pull --depth=100 origin vsc${saltTargetVersion}
-    & git checkout vsc${saltTargetVersion}
-    & git branch -d master
+    RunCmd -Cmd git.exe -ArgumentList "remote add origin https://github.com/gigi206/salt.git"
+    if (${env:VSCode_Anywhere_CI}) {
+        RunCmd -Cmd git.exe -ArgumentList "pull --quiet --depth=100 origin vsc${saltTargetVersion}"
+        RunCmd -Cmd git.exe -ArgumentList "checkout --quiet vsc${saltTargetVersion}"
+    }
+    else {
+        RunCmd -Cmd git.exe -ArgumentList "pull --depth=100 origin vsc${saltTargetVersion}"
+        RunCmd -Cmd git.exe -ArgumentList "checkout vsc${saltTargetVersion}"
+    }
+    RunCmd -Cmd git.exe -ArgumentList "branch -d master"
 }
 else {
-    Write-Host "* Syncing Saltstack ${saltCurrentVersion} to the latest VSCode-Anywhere updates (fork)" -ForegroundColor Cyan
+    Output "Syncing Saltstack ${saltCurrentVersion} to the latest VSCode-Anywhere updates (fork)"
     # Set-Location ((Resolve-Path "${PSScriptRoot}\{{ salt['vscode_anywhere.relpath'](salt['grains.get']('vscode-anywhere:tools:path'), salt['grains.get']('vscode-anywhere:saltstack:path')) }}").Path + '\bin\Lib\site-packages\salt-*')
     Set-Location ("{{ salt['vscode_anywhere.relpath'](salt['grains.get']('vscode-anywhere:tools:path'), salt['grains.get']('vscode-anywhere:saltstack:path')) }}" + '\bin\Lib\site-packages\salt-*')
-    git reset --hard HEAD
-    if (${env:VSCode_Anywhere_CI}) { git pull --quiet --ff-only origin vsc${saltTargetVersion} }
-    else { git pull --ff-only origin vsc${saltTargetVersion} }
+    RunCmd -Cmd git.exe -ArgumentList "reset --hard HEAD"
+    if (${env:VSCode_Anywhere_CI}) { RunCmd -Cmd git.exe -ArgumentList "pull --quiet --ff-only origin vsc${saltTargetVersion}" }
+    else { RunCmd -Cmd git.exe -ArgumentList "pull --ff-only origin vsc${saltTargetVersion}" }
 }
 
-Write-Host "* Updating VSCode-Anywhere" -ForegroundColor Cyan
+Output "Updating VSCode-Anywhere"
 Set-Location "${PSScriptRoot}"
 . "{{ salt['vscode_anywhere.relpath'](salt['grains.get']('vscode-anywhere:tools:path'), salt['grains.get']('vscode-anywhere:tools:env')) }}"
+# RunCmd -Cmd ".\vscode-anywhere.ps1" -ArgumentList "--retcode-passthrough --state-verbose=False --force-color state.apply salt/utils/update saltenv={{ saltenv }} sync_mods=all $Args"
 & .\vscode-anywhere.ps1 --retcode-passthrough --state-verbose=False --force-color state.apply salt/utils/update saltenv={{ saltenv }} sync_mods=all $Args
-$exit = $?
+$exit = ${LASTEXITCODE}
 
 if (!(${env:VSCode_Anywhere_CI})) { Pause }
-exit ${exit}
+
+exit $exit
